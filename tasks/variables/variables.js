1fac2cdc0cf164dd3376aa2441c7f9f95d143e95
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 <yerassyl.nurzhan@gmail.com>
2022-02-15 11:10:11 +0600

    corrected

--- tasks/variables/variables.js
+++ tasks/variables/variables.js
@@ -6,27 +6,27 @@
 /**
  * TODO create variable named int and set value equal to 1
  */
-let int = 1;
+const int = 1;
 
 /**
  *  TODO create variable named int2 and set value equal int plus 1
  */
-let int2 = int + 1;
+const int2 = int + 1;
 
 /**
  * TODO create variable named str and set value equal to "John"
  */
-let str = 'John';
+const str = 'John';
 
 /**
  * TODO create variable named str2 and set value equal str plus " Dou"
  */
-let str2 = str + ' ' + 'Dou';
+const str2 = str + ' Dou';
 
 /**
  * TODO create variable named bool and set value of equality comparison of int and int2 variables
  */
-let bool = int == int2;
+const bool = int === int2;
 
 /**
  * TODO create variable named arr and set value array of numbers from 1 to 5
@@ -36,12 +36,12 @@ const arr = [1,2,3,4,5];
 /**
  * TODO create variable named fifth and set value as element of array with index 4
  */
-let fifth = arr[4];
+const fifth = arr[4];
 
 /**
  * TODO create variable named human and set value as object with key firstName and value "John"
  */
-let human = {firstName: 'John'};
+const human = {firstName: 'John'};
 
 /**
  * TODO set new key of human object named lastName and value "Dou"
