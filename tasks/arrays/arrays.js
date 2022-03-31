b07229c4fcff05b65e802b486e45b116d8edbdd6
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 <yerassyl.nurzhan@gmail.com>
2022-03-31 13:04:06 +0600

    Hometask 5 Update 2

--- tasks/arrays/arrays.js
+++ tasks/arrays/arrays.js
@@ -1,16 +1,22 @@
+const { objDisplay } = require("chai/lib/chai/utils");
+
 /**
  * return an array of characters names
  * Avoid using forEach() method and for|for..of loops
  * @param {Array} chars
  * @return {Array} - char names
  */
-function getCharactersNames(chars) {}
+function getCharactersNames(chars) {
+	return chars.name;
+}
 
 /**
  * print (console.log) names of all characters
  * @param {Array} chars
  */
-function printCharacterNames(chars) {}
+function printCharacterNames(chars) {
+	console.log(chars.name);
+}
 
 /**
  * return an array of non-human (species !== 'human') characters
@@ -18,7 +24,10 @@ function printCharacterNames(chars) {}
  * @param {Array} chars
  * @return {Array} - non human characters
  */
-function getNonHumanCharacters(chars) {}
+function getNonHumanCharacters(chars) {
+	const nonHumanCharacters = chars.filter(chars => chars.species !== "human");
+	return nonHumanCharacters;
+}
 
 /**
  * return info about character named 'Jerry Smith'
@@ -26,7 +35,9 @@ function getNonHumanCharacters(chars) {}
  * @param {Array} chars
  * @return {Object} - Jerry object
  */
-function getJerryInfo(chars) {}
+function getJerryInfo(chars) {
+	return chars.filter(chars => chars.name == "Jerry Smith");
+}
 
 /**
  * check if all characters are human (species attribute). return true if all, false if not
@@ -34,7 +45,9 @@ function getJerryInfo(chars) {}
  * @param {Array} chars
  * @return {boolean}
  */
-function isAllHuman(chars) {}
+function isAllHuman(chars) {
+	return chars.includes("species" == "Human");
+}
 
 /**
  * check if there are any 'type == Fish-Person' characters. return true if any, false if not
@@ -42,7 +55,9 @@ function isAllHuman(chars) {}
  * @param {Array} chars
  * @return {boolean}
  */
-function isAnyFishPerson(chars) {}
+function isAnyFishPerson(chars) {
+	return chars.some(String => "type" === "Fish-Person");
+}
 
 /**
  * 1. Write a method to find an index of minimal item from an array;
@@ -52,7 +67,8 @@ function isAnyFishPerson(chars) {}
  * console.log(minItem([2,5,6,3,1,8])) // 4
  */
 function minItem(arr) {
-	//PLACE YOUR CODE HERE
+	let min = Math.min.apply(null, arr);
+	return arr.indexOf(min);
 }
 
 module.exports = {
